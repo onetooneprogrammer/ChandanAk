d6bdd8d337707791105bd095ec506a3bef609fa5
onetooneprogrammer <57839855+onetooneprogrammer@users.noreply.github.com>
2026-02-23 23:25:51 +0530

    Add celebrate function for displaying celebration message

---
+++ script.js
@@ -0,0 +1,6 @@
+function celebrate() {
+  const celebration = document.getElementById("celebration");
+  celebration.innerHTML = "💖💍🌸✨ She said YES! ✨🌸💍💖";
+  celebration.style.fontSize = "2em";
+  celebration.style.marginTop = "20px";
+}
